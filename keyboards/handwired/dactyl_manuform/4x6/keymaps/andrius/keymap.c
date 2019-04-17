af12bb5a2c82f034566ce48b8a2c8c6d793c41fd
etherandrius <etherandrius@gmail.com>
2019-04-17 21:21:01 +0300

    shift escape is a stupid idea in vim

--- keyboards/handwired/dactyl_manuform/4x6/keymaps/andrius/keymap.c
+++ keyboards/handwired/dactyl_manuform/4x6/keymaps/andrius/keymap.c
@@ -55,7 +55,6 @@ enum {
 
 // Switch to function layer when held, escape when tapped
 #define AD_ESC LT(LAYER_ADJUST, KC_ESCAPE)
-#define SF_ESC SFT_T(KC_ESC)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -65,7 +64,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|                  |------+------+------+------+------+------|
  * | ESC  |   a  |   s  |   d  |   f  |   g  |                  |   h  |   j  |   k  |   l  |   ;  |  '   |
  * |------+------+------+------+------+------|                  |------+------+------+------+------+------|
- * |SHIFT |   z  |   x  |   c  |   v  |   b  |                  |   n  |   m  |   ,  |   .  |   /  |SF/esc|
+ * |SHIFT |   z  |   x  |   c  |   v  |   b  |                  |   n  |   m  |   ,  |   .  |   /  |SHIFT |
  * +------+------+------+------+-------------+                  +-------------+------+------+------+------+
  *               |   -  |   +  |                                              | MOUSE|   \  |
  *               +-------------+-------------+                  +-------------+-------------+
@@ -80,7 +79,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [LAYER_QWERTY] = LAYOUT(
     KC_GRV ,  KC_Q,  KC_W,   KC_E,   KC_R,   KC_T,               KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_DEL,   \
     AD_ESC ,  KC_A,  KC_S,   KC_D,   KC_F,   KC_G,               KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,  \
-    KC_LSFT,  KC_Z,  KC_X,   KC_C,   KC_V,   KC_B,               KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,SF_ESC,   \
+    KC_LSFT,  KC_Z,  KC_X,   KC_C,   KC_V,   KC_B,               KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,KC_RSFT,   \
                      KC_MINUS,KC_EQL,                                             MOUSE  ,KC_BSLS,                 \
                                     KC_ENT , KC_SPC ,            KC_BSPC,  KC_TAB ,                                \
                                     KC_LCTL,  LOWER ,             RAISE ,  KC_LALT,                                \
