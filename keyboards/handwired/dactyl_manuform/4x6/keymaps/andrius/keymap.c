045969d5966297edba15d0ff73aaa16385598456
etherandrius <etherandrius@gmail.com>
2019-04-18 10:33:20 +0300

    swapped shift and gui

--- keyboards/handwired/dactyl_manuform/4x6/keymaps/andrius/keymap.c
+++ keyboards/handwired/dactyl_manuform/4x6/keymaps/andrius/keymap.c
@@ -72,7 +72,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                             |------+------|                  |------+------|
  *                             | CTRL |LOWER |                  |RAISE | ALT  |
  *                             |------+------|                  |------+------|
- *                             | SHFT |SUPER |                  | FN   | META |
+ *                             | META |SUPER |                  | FN   | SHFT |
  *                             +-------------+                  +-------------+
  */
 
@@ -83,7 +83,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                      KC_MINUS,KC_EQL,                                             MOUSE  ,KC_BSLS,                 \
                                     KC_ENT , KC_SPC ,            KC_BSPC,  KC_TAB ,                                \
                                     KC_LCTL,  LOWER ,             RAISE ,  KC_LALT,                                \
-                                    KC_LSFT,  TEST  ,              FN   ,  KC_LGUI                                 \
+                                    KC_LGUI,  TEST  ,              FN   ,  KC_LSFT                                 \
 ),
 
 /* Base (lower)
