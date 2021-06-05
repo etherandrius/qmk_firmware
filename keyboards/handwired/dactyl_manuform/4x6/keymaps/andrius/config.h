dba2546f53f503e2a7ed6ae09b834a0123e38a96
agrabauskas <agrabauskas@palantir.com>
2021-06-05 20:25:35 +0100

    symbol layer a thing now? making stuff prety

--- keyboards/handwired/dactyl_manuform/4x6/keymaps/andrius/config.h
+++ keyboards/handwired/dactyl_manuform/4x6/keymaps/andrius/config.h
@@ -38,7 +38,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define IGNORE_MOD_TAP_INTERRUPT
 
 // Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
-#define TAPPING_FORCE_HOLD
+// #define TAPPING_FORCE_HOLD
 
 // Apply the modifier on keys that are tapped during a short hold of a modtap
 #define PERMISSIVE_HOLD
--- keyboards/handwired/dactyl_manuform/4x6/keymaps/andrius/keymap.c
+++ keyboards/handwired/dactyl_manuform/4x6/keymaps/andrius/keymap.c
@@ -32,7 +32,6 @@ enum {
 #define SHIFT OSM(MOD_LSFT)
 
 // // Left-hand home row mods
-/* #define HOME_A LALT_T(KC_A) */
 #define HOME_S LALT_T(KC_S)
 #define HOME_D LCTL_T(KC_D)
 #define HOME_F CMD_T(KC_F)
@@ -40,10 +39,10 @@ enum {
 // Right-hand home row mods
 #define HOME_J CMD_T(KC_J)
 #define HOME_K RCTL_T(KC_K)
-#define HOME_L RALT_T(KC_L)
-/* #define HOME_SCLN LALT_T(KC_SCLN) */
+#define HOME_L LALT_T(KC_L)
 
 #define NUM_Z LT(LAYER_NUMBER, KC_Z)
+#define NAC_A LT(LAYER_NAV, KC_A)
 
 // Switch to function layer when held, escape when tapped
 #define AD_ESC LT(LAYER_ADJUST, KC_ESCAPE)
@@ -54,24 +53,49 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * +-----------------------------------------+                  +-----------------------------------------+
  * |      |   q  |   w  |   e  |   r  |   t  |                  |   y  |   u  |   i  |   o  |   p  |      |
  * |------+------+------+------+------+------|                  |------+------+------+------+------+------|
- * |Num   |a|ATL |s|CLT |d|SFT |f|CMD |   g  |                  |   h  |j|CMD |k|SFT |l|CLT |;|ATL | ENTR |
+ * | ESC  |a|NAV |s|ALT |d|CTL |f|CMD |   g  |                  |   h  |j|CMD |k|CTL |l|ALT |   ;  | ENTR |
  * |------+------+------+------+------+------|                  |------+------+------+------+------+------|
- * |      |   z  |   x  |   c  |   v  |   b  |                  |   n  |   m  |   ,  |   .  |   /  |      |
+ * |      |z|NUM |   x  |   c  |   v  |   b  |                  |   n  |   m  |      |      |      |      |
  * +------+------+------+------+-------------+                  +-------------+------+------+------+------+
- *               | NAV  | ALT  |                                              |      |   \  |
+ *               |      | ALT  |                                              |      |      |
  *               +-------------+--------------------+    +--------------------+-------------+
- *                             |SHIFT |SPACE | NAV  |    | NUM  | BCSP | SYM  |
+ *                             |SHIFT |SPACE |      |    |      | BCSP | SYM  |
  *                             +--------------------+    +--------------------+
  */
 [LAYER_QWERTY] = LAYOUT(
-    XXXXXXX,  KC_Q,  KC_W,  KC_E,  KC_R,   KC_T,               KC_Y,   KC_U,   KC_I,   KC_O,    KC_P,XXXXXXX,   \
-    AD_ESC ,  KC_A,HOME_S,HOME_D,HOME_F,   KC_G,               KC_H, HOME_J, HOME_K, HOME_L, KC_SCLN, KC_ENT,  \
-    XXXXXXX, NUM_Z,  KC_X,  KC_C,  KC_V,   KC_B,               KC_N,   KC_M,KC_COMM, KC_DOT, KC_SLSH,XXXXXXX,   \
-                     NAV ,KC_LALT,                                          KC_RCTL,KC_BSLS,                 \
-                                      SHIFT, KC_SPC,            KC_BSPC,   SYM  ,                                \
-                             OSL(LAYER_NAV),XXXXXXX,            XXXXXXX,OSL(LAYER_NUMBER),                   \
-                                    XXXXXXX,XXXXXXX,              FN   ,  KC_RALT                                 \
+    XXXXXXX,  KC_Q,  KC_W,  KC_E,  KC_R,   KC_T,               KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,XXXXXXX,   \
+    AD_ESC , NAV_A,HOME_S,HOME_D,HOME_F,   KC_G,               KC_H, HOME_J, HOME_K, HOME_L,KC_SCLN, KC_ENT,   \
+    XXXXXXX, NUM_Z,  KC_X,  KC_C,  KC_V,   KC_B,               KC_N,   KC_M,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,   \ // TEST remoe the pesky symbols on top layer
+ /* XXXXXXX, NUM_Z,  KC_X,  KC_C,  KC_V,   KC_B,               KC_N,   KC_M,KC_COMM, KC_DOT,KC_SLSH,XXXXXXX,   \ */
+                     NAV ,KC_LALT,                                          XXXXXXX,XXXXXXX,                   \
+                                      SHIFT, KC_SPC,            KC_BSPC,   SYM  ,                              \
+                             OSL(LAYER_NAV),XXXXXXX,            XXXXXXX,OSL(LAYER_NUMBER),                     \
+                                    XXXXXXX,XXXXXXX,              FN   ,  KC_RALT                              \
 ),
+
+/* Fake SHIFT
+ * +-----------------------------------------+                  +-----------------------------------------+
+ * |      | Q    | W    | E    | R    | T    |                  | Y    | U    | I    | O    | P    |      |
+ * |------+------+------+------+------+------|                  |------+------+------+------+------+------|
+ * | ESC  | A    | S    | D    | F    | G    |                  | H    | J    | K    | L    |      | ENTR |
+ * |------+------+------+------+------+------|                  |------+------+------+------+------+------|
+ * |      | Z    | X    | C    | V    | B    |                  | N    | M    |      |      |      |      |
+ * +------+------+------+------+-------------+                  +-------------+------+------+------+------+
+ *               |      |      |                                              |      |      |
+ *               +-------------+                                              +-------------+
+ */
+
+/*
+[LAYER_SHIFT] = LAYOUT(
+    _______,S(KC_Q),S(KC_W),S(KC_E),S(KC_R),S(KC_T),           S(KC_Y),S(KC_U),   S(KC_I),  S(KC_O),   S(KC_P),_______,  \
+    _______,S(KC_A),S(KC_S),S(KC_D),S(KC_F),S(KC_G),           S(KC_H),S(KC_J),   S(KC_K),  S(KC_L),S(KC_SCLN),_______,  \
+    _______,S(KC_Z),S(KC_X),S(KC_C),S(KC_V),S(KC_B),           S(KC_N),S(KC_M),S(KC_COMM),S(KC_DOT),S(KC_SLSH),_______,  \
+                   _______,_______,                                           _______,_______,                           \
+                                    _______,_______,           _______,_______,                                          \
+                                    _______,_______,           _______,_______,                                          \
+                                    _______,_______,           _______,_______                                           \
+),
+*/
 
 /* Navigation 
  * +-----------------------------------------+                  +-----------------------------------------+
@@ -85,36 +109,36 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *               +-------------+                                              +-------------+
  */
 [LAYER_NAV] = LAYOUT(
-    _______,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,            XXXXXXX,G(KC_LBRC),G(KC_RBRC),XXXXXXX,XXXXXXX,_______,  \
-    _______,XXXXXXX,KC_LALT,KC_LCTL,KC_LCMD,XXXXXXX,            KC_LEFT,KC_DOWN, KC_UP ,KC_RIGHT,XXXXXXX,_______, \
-    _______,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,            KC_PGDOWN,KC_WH_U,KC_WH_D,KC_PGUP,XXXXXXX,_______,  \
-                    _______,_______,                                            XXXXXXX,XXXXXXX,                   \
-                                    _______,_______,            _______,_______,                                   \
-                                    _______,XXXXXXX,            _______,_______,                                   \
-                                    _______,_______,            _______,_______                                    \
+    _______,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,            XXXXXXX  ,G(KC_LBRC),G(KC_RBRC),XXXXXXX ,XXXXXXX,_______,  \
+    _______,XXXXXXX,KC_LALT,KC_LCTL,KC_LCMD,XXXXXXX,            KC_LEFT  ,  KC_DOWN ,  KC_UP   ,KC_RIGHT,XXXXXXX,_______,  \
+    _______,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,            KC_PGDOWN,  KC_WH_U ,  KC_WH_D ,KC_PGUP ,XXXXXXX,_______,  \
+                    _______,_______,                                            XXXXXXX,XXXXXXX,                           \
+                                    _______,_______,            _______,_______,                                           \
+                                    _______,XXXXXXX,            _______,_______,                                           \
+                                    _______,_______,            _______,_______                                            \
 ),
 
-/* Symbol
+/* Symbol   
  * |------+------+------+------+------+------|                  |------+------+------+------+------+------|
- * |      |      |      |  !   |  ^   |  ~   |                  |  "   |  [   |  ]   |  +   |   =  |      |
+ * |      |  ?   |  /   |  !   |  ^   |  ~   |                  |  "   |  [   |  ]   |  +   |   =  |      |
  * |------+------+------+------+------+------|                  |------+------+------+------+------+------|
  * |      |  @   |  *   |  $   |  %   |  `   |                  |  '   |  (   |  )   |  -   |   :  | TAB  |
  * |------+------+------+------+------+------|                  |------+------+------+------+------+------|
- * |      |      |      |  &   |  #   |      |                  |  |   |  {   |  }   |  _   |   \  |      |
+ * |      |      |  &   |  <   |  >   |  #   |                  |  |   |  {   |  }   |  _   |   \  |      |
  * +------+------+------+------+-------------+                  +-------------+------+------+------+------+
  *               |      |      |                                              |      |      |
  *               +-------------+--------------------+    +--------------------+-------------+
- *                             |      |      |      |    |      |      |  NAV |
+ *                             |   ,  |  .   |      |    |      |      |  NAV |
  *                             +--------------------+    +--------------------+
  */
 [LAYER_SYMBOLS] = LAYOUT(
-    _______,XXXXXXX,XXXXXXX,S(KC_1),S(KC_6),S(KC_GRV),         S(KC_QUOT),KC_LBRC,KC_RBRC,S(KC_EQL),KC_EQL,KC_CAPS,  \
-    XXXXXXX,S(KC_2),S(KC_8),S(KC_4),S(KC_5),KC_GRV ,            KC_QUOT,S(KC_9),S(KC_0),KC_MINUS,S(KC_SCLN),KC_TAB,  \
-    _______,XXXXXXX,XXXXXXX,S(KC_7),S(KC_3),XXXXXXX,           S(KC_BSLS),KC_LCBR,KC_RCBR,S(KC_MINUS),XXXXXXX,_______,  \
-                    XXXXXXX,XXXXXXX,                                            S(KC_COMM),S(KC_DOT),                   \
-                                    _______,_______,            _______,NAV,                                   \
-                                    _______,_______,            XXXXXXX,_______,                                   \
-                                    _______,_______,            _______,_______                                    \
+    _______,KC_SLSH,S(KC_SLSH),S(KC_1),  S(KC_6),S(KC_GRV),         S(KC_QUOT),KC_LBRC,KC_RBRC,  S(KC_EQL),   KC_EQL ,_______,  \
+    _______,S(KC_2),S(KC_8),   S(KC_4),  S(KC_5),  KC_GRV             KC_QUOT ,S(KC_9),S(KC_0),  KC_MINUS ,S(KC_SCLN),KC_TAB ,  \
+    _______,XXXXXXX,S(KC_7),S(KC_COMM),S(KC_DOT),  S(KC_3),         S(KC_BSLS),KC_LCBR,KC_RCBR,S(KC_MINUS),  KC_BSLS ,_______,  \
+                    XXXXXXX,XXXXXXX,                                            KC_TAB,XXXXXXX,                                 \
+                                    KC_COMM, KC_DOT,            _______,NAV,                                                    \
+                                    _______,_______,            XXXXXXX,_______,                                                \
+                                    _______,_______,            _______,_______                                                 \
 ),
 
 /* Function and media controls
@@ -159,10 +183,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______,_______,_______,_______,_______,_______,            _______,XXXXXXX,XXXXXXX,XXXXXXX,_______,KC_CRST,   \
     _______,_______,_______,_______,_______,_______,            _______,XXXXXXX,XXXXXXX,XXXXXXX,_______,KC_CRST,   \
     _______,_______,_______,_______,_______,_______,            _______,XXXXXXX,XXXXXXX,XXXXXXX,_______,KC_CRST,   \
-                    _______,_______,                                         XXXXXXX,_______,                \
-                                    _______,_______,            _______,_______,                          \
-                                    _______,XXXXXXX,            XXXXXXX,_______,                          \
-                                    _______,_______,            _______,_______                           \
+                    _______,_______,                                         XXXXXXX,_______,                      \
+                                    _______,_______,            _______,_______,                                   \
+                                    _______,XXXXXXX,            XXXXXXX,_______,                                   \
+                                    _______,_______,            _______,_______                                    \
 )
 };
 
